9ba3be11fbf67b090695dfe27f322e45a2ba3ccb
tstibbs <tstibbs@users.noreply.github.com>
2016-05-25 20:17:28 +0100

    Cli

---
+++ cli.js
@@ -0,0 +1,10 @@
+// run using 'node node_modules/lcov-zeroer/cli.js ./coverage/lcov.info ./coverage/lcov.info.baseline'
+
+var zeroer = require('./index.js')
+
+var inputFile = process.argv[2];
+var outputFile = process.argv[3];
+
+zeroer(inputFile, outputFile, function() {
+	console.log('Finished zeroing file ' + file);
+});
